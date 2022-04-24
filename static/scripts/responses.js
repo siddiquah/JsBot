cc549fe75e37fd7c3dcfdccc28d039a609a109ee
siddiquah anjum <siddiquahanjum111@gmail.com>
2022-04-24 08:49:11 +0530

    Commit

---
+++ static/scripts/responses.js
@@ -0,0 +1,38 @@
+function getBotResponse(input) {
+    if(input == "hello") {
+        return "what do you want?"
+    }
+    else if(input == "tell me a joke") {
+        return "Sure, Why did the chatbot cross the road"  
+    }
+    else if(input == "why") {
+        return "Because it was programmed to be a chicken!"
+    }
+    else if(input == "What’s up?") {
+        return "Ceiling"
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Youre so beautiful, bot") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+    else if(input == "Are you hungry?") {
+        return "No thanks, I just had a byte to eat."
+    }
+}
